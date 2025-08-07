e61661c25e55c40bf68ce4a665ddab980780d136
Milik <miliksmith23@gmail.com>
2025-08-07 11:29:46 -0400

    Add dependencies and initial linting

--- personalcollectiontracker/eslint.config.mjs
+++ personalcollectiontracker/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from 'eslint-plugin-perfectionist'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,23 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    ignorePatterns: ["**/__generated__/**"],
+    extends: ["next/core-web-vitals", "next/typescript"],
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      'perfectionist/sort-imports': "error",
+      'perfectionist/sort-classes': "error",
+      'perfectionist/sort-exports': "error",
+      'perfectionist/sort-jsx-props': "error",
+      'perfectionist/sort-interfaces': "error",
+      'perfectionist/sort-named-exports': "error",
+      'perfectionist/sort-named-imports': "error",
+      'perfectionist/sort-objects': "error",
+    },
+  }),
 ];
 
 export default eslintConfig;
---
+++ personalcollectiontracker/prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  bracketSameLine: false,
+  bracketSpacing: true,
+};
+
+export default config;
