bd7ebb0e700a2326c224028b5f1dc39146047fde
MilikSmith76 <miliksmith23@gmail.com>
2025-09-26 21:47:36 -0400

    Update brands page to display brands from API endpoint

--- personalcollectiontracker/src/app/brands/page.tsx
+++ personalcollectiontracker/src/app/brands/page.tsx
@@ -1,31 +1,40 @@
-import { JSX } from 'react';
+'use client';
+import axios from 'axios';
+import Link from 'next/link';
+import { JSX, useCallback, useEffect, useState } from 'react';
 
-import { Button, Header, Main, Spacer, Table } from '@/components';
+import { Header, Main, Spacer, Table } from '@/components';
 import Pagination from '@/components/pagination';
 
-const options = ['id', 'name'];
-
-const data = [
-    {
-        id: 1,
-        name: 'Brand 1',
-    },
-    {
-        id: 2,
-        name: 'Brand 2',
-    },
-];
+const OPTIONS = ['id', 'name'];
 
 const BrandPage = (): JSX.Element => {
+    const [brands, setBrands] = useState([]);
+
+    const fetchBrands = useCallback(async () => {
+        const result = await axios.get('/api/brands');
+
+        setBrands(result.data.brands);
+    }, [setBrands]);
+
+    useEffect(() => {
+        fetchBrands();
+    }, [fetchBrands]);
+
     return (
         <>
             <Header name='Brands' />
             <Main>
                 <Spacer y={20} />
-                <div className='mr-auto ml-auto w-2/3 text-right'>
-                    <Button classNameExtension='mr-0' text='Create' />
+                <div className='mr-auto mb-5 ml-auto w-2/3 text-right'>
+                    <Link
+                        className='m-2 rounded bg-sky-600 px-4 py-3 hover:bg-sky-500'
+                        href='/brands/new'
+                    >
+                        Create
+                    </Link>
                 </div>
-                <Table data={data} labels={options} />
+                <Table data={brands} labels={OPTIONS} />
                 <div className='mt-2 mr-auto ml-auto w-2/3 text-center'>
                     <Pagination />
                 </div>
